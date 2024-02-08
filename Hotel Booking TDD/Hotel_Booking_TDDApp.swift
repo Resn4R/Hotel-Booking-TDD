d9d096f42ed738c741edb209cc69c04e173afed0
Vito <purview-joust.0x@icloud.com>
2024-02-08 18:14:06 +0000

    Initial Commit

---
+++ Hotel Booking TDD/Hotel_Booking_TDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hotel_Booking_TDDApp.swift
+//  Hotel Booking TDD
+//
+//  Created by Vito Borghi on 08/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Hotel_Booking_TDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
